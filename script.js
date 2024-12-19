e753411668a8d393fa5bf6bb42c9419a27553595
rTCollins-dev <1realcollins@gmail.com>
2024-12-19 10:40:28 +0000

    Add files via upload

---
+++ script.js
@@ -0,0 +1,18 @@
+var btc = document.getElementById("bitcoin");
+var eth = document.getElementById("ethereum");
+var ltc = document.getElementById("litecoin");
+
+
+
+var settings = {
+    "async": true,
+    "scrossDomain": true,
+    "url": "https://api.coingecko.com/api/v3/simple/price?ids=bitcoin%2Cethereum%2Clitecoin&vs_currencies=usd",
+    "method": "GET",
+    "headers": {}
+}
+$.ajax(settings).done(function (response){
+    btc.innerHTML = response.bitcoin.usd;
+    eth.innerHTML = response.ethereum.usd;
+    ltc.innerHTML = response.litecoin.usd;
+})
